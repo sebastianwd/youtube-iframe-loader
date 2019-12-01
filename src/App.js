8d2cbad351912cd3860cb2260e0557a4e8c611bb
Sebastian <sebastian.luque.pl@gmail.com>
2019-12-01 17:08:04 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams
+} from "react-router-dom";
+import "./App.css";
+
+function IframeLoader() {
+  let { slug } = useParams();
+  return (
+    <div
+      style={{
+        position: "fixed",
+        overflowY: "hidden",
+        overflowX: "hidden",
+        width: "100%",
+        height: "100%"
+      }}>
+      <iframe
+        style={{ width: "100%", height: "100%" }}
+        src={`https://www.youtube.com/embed/${slug}?&autoplay=0&playsinline=1`}
+        playsInline='1'
+        frameBorder='0'
+        scrolling='no'
+        allow='playsinline; accelerometer; encrypted-media; gyroscope; picture-in-picture'
+        allowFullScreen
+      />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <div>try adding a youtube id as a route param</div>
+        </Route>
+        <Route path='/:slug'>
+          <IframeLoader></IframeLoader>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
